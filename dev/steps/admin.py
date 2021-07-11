9aa4c5670b22cb6678665cdac0fff1206b6bad56
Yohei_Hamada <2ykgh6@gmail.com>
2021-07-11 16:42:19 +0900

    Add create models

--- dev/steps/admin.py
+++ dev/steps/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Tracker, Tracking_item
 
 # Register your models here.
+
+admin.site.register(Tracker)
+admin.site.register(Tracking_item)
--- dev/steps/models.py
+++ dev/steps/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Tracking_item(models.Model):
+    username = models.TextField()
+    tracking_item_id = models.CharField(max_length=8)
+    tracking_item_name = models.TextField()
+    data_type = models.IntegerField()
+
+    def __str__(self):
+        return '{}_{}_{}_{}'.format(self.username, self.tracking_item_id, self.tracking_item_name, self.data_type)
+
+class Tracker(models.Model):
+    username = models.TextField()
+    year = models.CharField(max_length=4)
+    month = models.CharField(max_length=2)
+    day = models.CharField(max_length=2)
+    tracking_item_id = models.CharField(max_length=8)
+    tracking_value = models.TextField()
+    tracking_items = models.ForeignKey(Tracking_item, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{}_{}_{}_{}_{}_{}_{}'.format(self.username, self.year, self.month, self.day, self.tracking_item_id, self.tracking_value, self.tracking_items)
